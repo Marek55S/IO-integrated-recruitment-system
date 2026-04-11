a16be8d5de817ebd613c477355340a172a715244
Bartosz Ludwin <93099232+FalcO0O@users.noreply.github.com>
2026-04-12 00:04:13 +0200

    add endpoints

---
+++ client/lib/db.ts
@@ -0,0 +1,19 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export async function query(text: string, params?: any[]) {
+  const start = Date.now();
+  const res = await pool.query(text, params);
+  const duration = Date.now() - start;
+
+  console.log('[Baza Danych] Wykonano zapytanie', {
+    text,
+    duration,
+    rows: res.rowCount,
+  });
+
+  return res;
+}
